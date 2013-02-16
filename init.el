4dce78e5819541f82496ec66359da770d1767df9
Tim Richards <tim.d.richards@gmail.com>
2013-02-16 10:23:31 -0500

    added init.el; markdown-mode

---
+++ init.el
@@ -0,0 +1,9 @@
+
+; disable backup files
+(setq make-backup-files nil)
+
+; add markdown-mode
+(add-to-list 'load-path "~/.emacs.d/markdown-mode")
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
