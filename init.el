a9187988e860377f797a61cc0504ce49275507bb
Tim Richards <tim.d.richards@gmail.com>
2013-02-16 10:25:26 -0500

    changed auto-load directory for markdown

--- init.el
+++ init.el
@@ -3,7 +3,7 @@
 (setq make-backup-files nil)
 
 ; add markdown-mode
-(add-to-list 'load-path "~/.emacs.d/markdown-mode")
+(add-to-list 'load-path "~/.emacs.d/plugins/markdown-mode")
 (autoload 'markdown-mode "markdown-mode"
    "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
