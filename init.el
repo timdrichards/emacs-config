80e543cbcaa42aeecc31daebafb038949bcdf95e
Tim Richards <tim.d.richards@gmail.com>
2013-02-16 11:06:36 -0500

    added melpa package support

--- init.el
+++ init.el
@@ -1,9 +1,20 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
 
 ; disable backup files
 (setq make-backup-files nil)
 
-; add markdown-mode
-(add-to-list 'load-path "~/.emacs.d/plugins/markdown-mode")
+; markdown-mode
 (autoload 'markdown-mode "markdown-mode"
    "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+; auto-complete-mode
+(require 'auto-complete-config)
+(setq-default ac-sources (add-to-list 'ac-sources 'ac-source-dictionary))
+(global-auto-complete-mode t)
+(setq ac-auto-start 2)
+(setq ac-ignore-case nil)
+
