e948b8bc1d2eb2e52d2f674599a310f5579f26a7
Tim Richards <tim.d.richards@gmail.com>
2014-09-29 10:26:24 -0400

    yasnippet and auto-complete now work nicely together!

--- init.el
+++ init.el
@@ -15,8 +15,27 @@
         (package-refresh-contents)
         (require-package package min-version t)))))
 
+;; Auto-install missing packages:
 (require-package 'yasnippet)
-(require-package 'auto-complete)       ;; auto completion
+(require-package 'auto-complete)
+(require-package 'ac-dabbrev)
 (require-package 'magit)
 (require-package 'smartparens)
 
+;;; yasnippet
+;;; should be loaded before auto complete so that they can work together
+(require 'yasnippet)
+(yas-global-mode 1)
+
+;;; auto complete mod
+;;; should be loaded after yasnippet so that they can work together
+(require 'auto-complete)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/dict")
+(require 'auto-complete-config)
+(ac-config-default)
+
+;;; set the trigger key so that it can work together with yasnippet on tab key,
+;;; if the word exists in yasnippet, pressing tab will cause yasnippet to
+;;; activate, otherwise, auto-complete will
+(ac-set-trigger-key "TAB")
+(ac-set-trigger-key "<tab>")
