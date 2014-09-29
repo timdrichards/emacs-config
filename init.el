36d6bad630b5dc6213c9232e686afbaba1cd34d7
Tim Richards <tim.d.richards@gmail.com>
2014-09-29 10:11:14 -0400

    added new init.el file

---
+++ init.el
@@ -0,0 +1,22 @@
+(require 'package)
+(add-to-list 'package-archives
+  '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+(setq backup-directory-alist `(("." . "~/.saves")))
+
+(defun require-package (package &optional min-version no-refresh)
+  "Ask elpa to install given PACKAGE."
+  (if (package-installed-p package min-version)
+      t
+    (if (or (assoc package package-archive-contents) no-refresh)
+        (package-install package)
+      (progn
+        (package-refresh-contents)
+        (require-package package min-version t)))))
+
+(require-package 'yasnippet)
+(require-package 'auto-complete)       ;; auto completion
+(require-package 'magit)
+(require-package 'smartparens)
+
